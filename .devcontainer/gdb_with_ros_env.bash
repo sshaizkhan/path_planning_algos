a31fe93a33f2151b64e4f95614c897ebff4ad998
Shahwaz Khan <sshaizkhan@gmail.com>
2022-09-09 02:07:24 -0400

    (modify): gdb debugging setup

--- .devcontainer/gdb_with_ros_env.bash
+++ .devcontainer/gdb_with_ros_env.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Source our environment
-source /moveit2_app/moveit2_app/install/setup.bash
+source /home/bot/path_planner_ws/install/setup.bash
 
 # Run gdb, forwarding all args through
 /usr/bin/gdb ${@}
